79fc2e6114043713f3561265421d45cc8b78d812
muhammad-danish <totalnetsolution2@gmail.com>
2024-08-04 23:14:59 +0500

    index file modified

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC4a6vvBL4hgltPn8CJCQ8LdP9huBcFte4",
+  authDomain: "fir-curdapp-a02b5.firebaseapp.com",
+  projectId: "firebasecurdapp",
+  storageBucket: "firebasecurdapp.appspot.com",
+  messagingSenderId: "106845674234",
+  appId: "1:106845674234:web:e792104886ac97e009c743"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
